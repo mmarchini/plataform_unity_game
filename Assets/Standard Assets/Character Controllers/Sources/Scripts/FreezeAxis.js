261f6253e2d79108b3baee2570874e1d897bcaa9
Matheus Marchini <matheusdot@gmail.com>
2013-10-14 11:10:53 -0300

    This commit implements the basic movements
    Resolved #1

---
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/FreezeAxis.js
@@ -0,0 +1,24 @@
+﻿//using UnityEngine;
+//using System.Collections;
+ 
+public var FreezeX = false;
+public var FreezeY = false;
+public var FreezeZ = false;
+private var m_OriginPos : Vector3;   
+
+function Start ()
+    {
+        m_OriginPos = transform.position;
+    }
+ 
+function Update ()
+{
+    var currentPos : Vector3 = transform.position;
+    if(FreezeX)
+        currentPos.x = m_OriginPos.x;
+    if(FreezeY)
+        currentPos.y = m_OriginPos.y;
+    if(FreezeZ)
+        currentPos.z = m_OriginPos.z;
+    transform.position = currentPos;
+}
